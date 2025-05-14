ef36bad2952fe2441d44ccc798f4b9d814bf3365
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-05-14 11:14:14 -0300

    add decorator pattern

--- gof-patterns/behavioral/src/main/java/com/dsantos/templatemethod/Template.java
+++ gof-patterns/behavioral/src/main/java/com/dsantos/templatemethod/Template.java
@@ -1,4 +1,17 @@
 package com.dsantos.templatemethod;
 
 public class Template {
+
+    public static void main(String[] args) {
+
+        HouseTemplate houseType = new WoodenHouse();
+
+        //using template method
+        houseType.buildHouse();
+        System.out.println("************");
+
+        HouseTemplate glassHouse = new GlassHouse();
+
+        glassHouse.buildHouse();
+    }
 }
