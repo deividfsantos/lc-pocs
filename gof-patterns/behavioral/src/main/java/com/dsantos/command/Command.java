e7579bf41b949d3e89b8dd6c24cd2a32ba8954b2
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-06-17 00:24:41 -0300

    finished command pattern

---
+++ gof-patterns/behavioral/src/main/java/com/dsantos/command/Command.java
@@ -0,0 +1,6 @@
+package com.dsantos.command;
+
+public interface Command {
+
+    void execute();
+}
--- gof-patterns/behavioral/src/main/java/com/dsantos/command/Main.java
+++
@@ -1,8 +0,0 @@
-package com.dsantos.command;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("dgdgds");
-    }
-}
