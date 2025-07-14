ff7a2cbb4d3999aa44ad5d818d617785bc935122
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-07-13 23:03:06 -0300

    add complete channel to iterator

---
+++ gof-patterns/behavioral/src/main/java/com/dsantos/iterator/ChannelCollection.java
@@ -0,0 +1,11 @@
+package com.dsantos.iterator;
+
+public interface ChannelCollection {
+
+    public void addChannel(Channel c);
+
+    public void removeChannel(Channel c);
+
+    public ChannelIterator iterator(ChannelTypeEnum type);
+
+}
---
+++ gof-patterns/behavioral/src/main/java/com/dsantos/iterator/ChannelTypeEnum.java
@@ -0,0 +1,6 @@
+package com.dsantos.iterator;
+
+public enum ChannelTypeEnum {
+
+    ENGLISH, HINDI, FRENCH, ALL;
+}
