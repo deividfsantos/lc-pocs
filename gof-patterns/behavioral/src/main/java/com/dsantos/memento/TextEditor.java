910804672a0934c556d344a494b7f20abbff4743
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-07-19 23:35:08 -0300

    start memento pattern

---
+++ gof-patterns/behavioral/src/main/java/com/dsantos/memento/TextEditor.java
@@ -0,0 +1,10 @@
+package com.dsantos.memento;
+
+public class TextEditor {
+
+    private TextWindow textWindow;
+
+    public TextEditor(TextWindow textWindow) {
+        this.textWindow = textWindow;
+    }
+}
