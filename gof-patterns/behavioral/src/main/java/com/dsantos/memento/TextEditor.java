954cca2c283615891ad5fc08649477331839a5ff
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-07-20 23:39:24 -0300

    memento pattern almost finished

--- gof-patterns/behavioral/src/main/java/com/dsantos/memento/TextEditor.java
+++ gof-patterns/behavioral/src/main/java/com/dsantos/memento/TextEditor.java
@@ -3,8 +3,17 @@ package com.dsantos.memento;
 public class TextEditor {
 
     private TextWindow textWindow;
+    private TextWindowState savedTextWindow;
 
     public TextEditor(TextWindow textWindow) {
         this.textWindow = textWindow;
+    }
+
+    public void hitSave() {
+        savedTextWindow = textWindow.save();
+    }
+
+    public void hitUndo() {
+        textWindow.restore(savedTextWindow);
     }
 }
--- gof-patterns/behavioral/src/main/java/com/dsantos/memento/TextWindow.java
+++ gof-patterns/behavioral/src/main/java/com/dsantos/memento/TextWindow.java
@@ -11,4 +11,12 @@ public class TextWindow {
     public void addText(String text) {
         currentText.append(text);
     }
+
+    public TextWindowState save() {
+        return new TextWindowState(currentText.toString());
+    }
+
+    public void restore(TextWindowState save) {
+        currentText = new StringBuilder(save.getText());
+    }
 }
