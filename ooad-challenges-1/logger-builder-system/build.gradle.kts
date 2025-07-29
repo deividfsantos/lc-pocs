0f75fc2622499ffb678ea55458b361517cec949d
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-07-29 01:45:31 -0300

    fix gradle impl

--- ooad-challenges-1/logger-builder-system/build.gradle.kts
+++ ooad-challenges-1/logger-builder-system/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    java
+    application
 }
 
 group = "com.dsantos"
@@ -10,8 +11,16 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+application {
+    mainClass.set("com.dsantos.logger.demo.GradleDemo")
 }
 
 tasks.test {
