c5b3aa5eef72f9922c9b4494e3f18458b41de2b0
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-09-08 16:53:33 -0300

    add classes to render template POC

---
+++ ooad-challenges-1/render-template/src/main/java/com/dsantos/Template.java
@@ -0,0 +1,6 @@
+package com.dsantos;
+
+import java.util.Map;
+
+public record Template(String name, Map<String, Object> data) {
+}
---
+++ ooad-challenges-1/render-template/src/main/java/com/dsantos/TemplateEngine.java
@@ -0,0 +1,7 @@
+package com.dsantos;
+
+public class TemplateEngine {
+    public enum RenderFormat {
+        HTML, PDF, CSV
+    }
+}
