a8ee0a5026e762b6b6712e83818c4ac0c01020ae
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-09-20 00:28:29 -0300

    implement csv renderer

--- ooad-challenges-1/render-template/src/main/java/com/dsantos/renderer/CsvRenderer.java
+++ ooad-challenges-1/render-template/src/main/java/com/dsantos/renderer/CsvRenderer.java
@@ -2,14 +2,26 @@ package com.dsantos.renderer;
 
 import com.dsantos.Template;
 
-public class CsvRenderer implements TemplateRenderer{
+import java.util.Map;
+
+public class CsvRenderer implements TemplateRenderer {
     @Override
     public String render(Template template) {
-        return "";
+        StringBuilder csvBuilder = new StringBuilder();
+        csvBuilder.append("Property,Value\n");
+
+        for (Map.Entry<String, Object> entry : template.data().entrySet()) {
+            csvBuilder.append(entry.getKey())
+                    .append(",")
+                    .append(entry.getValue())
+                    .append("\n");
+        }
+
+        return csvBuilder.toString();
     }
 
     @Override
     public String getFileExtension() {
-        return "";
+        return "csv";
     }
 }
