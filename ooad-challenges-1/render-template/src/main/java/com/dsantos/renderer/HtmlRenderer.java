d61d195e241b84661f7201cd41c8c0cf9c50fb02
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-09-22 00:32:24 -0300

    implement pdf renderer

--- ooad-challenges-1/render-template/src/main/java/com/dsantos/renderer/HtmlRenderer.java
+++ ooad-challenges-1/render-template/src/main/java/com/dsantos/renderer/HtmlRenderer.java
@@ -5,11 +5,15 @@ import com.dsantos.Template;
 public class HtmlRenderer implements TemplateRenderer {
     @Override
     public String render(Template template) {
-        return "";
+        StringBuilder html = new StringBuilder();
+        html.append("<html><head><title>")
+            .append(template.name())
+            .append("</title></head><body>");
+        return html.toString();
     }
 
     @Override
     public String getFileExtension() {
-        return "";
+        return "html";
     }
 }
--- ooad-challenges-1/render-template/src/main/java/com/dsantos/renderer/PdfRenderer.java
+++ ooad-challenges-1/render-template/src/main/java/com/dsantos/renderer/PdfRenderer.java
@@ -5,11 +5,16 @@ import com.dsantos.Template;
 public class PdfRenderer implements TemplateRenderer{
     @Override
     public String render(Template template) {
-        return "";
+        StringBuilder pdf = new StringBuilder();
+        pdf.append("PDF Report: ").append(template.name()).append("\n\n");
+        for (var entry : template.data().entrySet()) {
+            pdf.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+        }
+        return pdf.toString();
     }
 
     @Override
     public String getFileExtension() {
-        return "";
+        return "pdf";
     }
 }
