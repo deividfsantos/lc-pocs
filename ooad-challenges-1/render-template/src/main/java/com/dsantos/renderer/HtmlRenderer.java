f3b8edd3361a2c4db7513fef6e850e3353b9b14b
deividfsantos <deividfranciscosantos@hotmail.com>
2025-09-28 22:54:31 -0300

    finished HtmlRenderer.java

--- ooad-challenges-1/render-template/src/main/java/com/dsantos/renderer/HtmlRenderer.java
+++ ooad-challenges-1/render-template/src/main/java/com/dsantos/renderer/HtmlRenderer.java
@@ -3,12 +3,28 @@ package com.dsantos.renderer;
 import com.dsantos.Template;
 
 public class HtmlRenderer implements TemplateRenderer {
+
     @Override
     public String render(Template template) {
         StringBuilder html = new StringBuilder();
-        html.append("<html><head><title>")
-            .append(template.name())
-            .append("</title></head><body>");
+        
+        html.append("<!DOCTYPE html>\n");
+        html.append("<html>\n");
+        html.append("<head>\n");
+        html.append("    <title>").append(template.getName()).append("</title>\n");
+        html.append("</head>\n");
+        html.append("<body>\n");
+        html.append("    <h1>").append(template.getName()).append("</h1>\n");
+        html.append("    <table border='1'>\n");
+        
+        for (Map.Entry<String, Object> entry : template.getData().entrySet()) {
+            html.append("        <tr><td>").append(entry.getKey()).append("</td>")
+                .append("<td>").append(entry.getValue()).append("</td></tr>\n");
+        }
+        
+        html.append("    </table>\n");
+        html.append("</body>\n");
+        html.append("</html>");
         return html.toString();
     }
 
