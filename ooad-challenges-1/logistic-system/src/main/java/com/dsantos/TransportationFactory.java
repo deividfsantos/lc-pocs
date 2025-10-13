63c86c75353dc2cfca8380693d5b04776fe951cd
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-10-12 22:51:11 -0300

    add logistic factory

--- ooad-challenges-1/logistic-system/src/main/java/com/dsantos/TransportationFactory.java
+++ ooad-challenges-1/logistic-system/src/main/java/com/dsantos/TransportationFactory.java
@@ -1,4 +1,18 @@
 package com.dsantos;
 
+import com.dsantos.transportation.BoatTransportation;
+import com.dsantos.transportation.RailTransportation;
+import com.dsantos.transportation.TransportationStrategy;
+import com.dsantos.transportation.TruckTransportation;
+
 public class TransportationFactory {
+
+    public static TransportationStrategy getTransportationStrategy(String type) {
+        return switch (type.toLowerCase()) {
+            case "truck" -> new TruckTransportation();
+            case "rail" -> new RailTransportation();
+            case "boat" -> new BoatTransportation();
+            default -> throw new IllegalArgumentException("Unknown transportation type: " + type);
+        };
+    }
 }
