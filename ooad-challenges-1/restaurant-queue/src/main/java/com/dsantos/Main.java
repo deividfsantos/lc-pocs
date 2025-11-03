8f417824eb0a94989af004929f5053bc13c4ab34
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-11-02 23:22:30 -0300

    add main class

--- ooad-challenges-1/restaurant-queue/src/main/java/com/dsantos/Main.java
+++ ooad-challenges-1/restaurant-queue/src/main/java/com/dsantos/Main.java
@@ -1,17 +1,26 @@
 package com.dsantos;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        RestaurantQueue queue = new RestaurantQueue();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        queue.enqueue(new Dish("Salad", 5));
+        queue.enqueue(new Dish("Soup", 10));
+        queue.enqueue(new Dish("Steak", 20));
+        queue.enqueue(new Dish("Fries", 8));
+
+        System.out.println("Current queue with estimates (startsIn / readyIn):");
+        for (RestaurantQueue.DishEstimate est : queue.estimates()) {
+            System.out.println(" - " + est);
+        }
+
+        System.out.println();
+        System.out.println("Serving next dish: " + queue.dequeue());
+
+        System.out.println();
+        System.out.println("Updated queue with estimates:");
+        for (RestaurantQueue.DishEstimate est : queue.estimates()) {
+            System.out.println(" - " + est);
         }
     }
 }
