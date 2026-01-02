abf58fbe515475e3b260b76e670bec68073e6331
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-01-02 01:26:40 -0300

    finished venue

---
+++ ooad-challenges-1/ticket-system/src/main/java/com/dsantos/Venue.java
@@ -0,0 +1,37 @@
+package com.dsantos;
+
+import java.util.*;
+
+public class Venue {
+    private final String name;
+    private final Map<Zone, List<Seat>> zones;
+
+    public Venue(String name, Map<Zone, Integer> zoneCapacities) {
+        this.name = name;
+        this.zones = new LinkedHashMap<>();
+
+        for (var entry : zoneCapacities.entrySet()) {
+            var zone = entry.getKey();
+            int capacity = entry.getValue();
+            var seats = new ArrayList<Seat>();
+            for (int i = 1; i <= capacity; i++) {
+                seats.add(new Seat(i, zone));
+            }
+            zones.put(zone, seats);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getTotalCapacity() {
+        return zones.values().stream().mapToInt(List::size).sum();
+    }
+
+    public Optional<Seat> getSeat(Zone zone, int seatNumber) {
+        return zones.get(zone).stream()
+                .filter(seat -> seat.number() == seatNumber)
+                .findFirst();
+    }
+}
