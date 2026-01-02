be36716a0be4ff70d0e51f545e14dfa8eb6a5646
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-01-02 01:39:04 -0300

    clean ticket system

--- ooad-challenges-1/ticket-system/src/main/java/com/dsantos/Ticket.java
+++ ooad-challenges-1/ticket-system/src/main/java/com/dsantos/Ticket.java
@@ -1,10 +1,12 @@
 package com.dsantos;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record Ticket(UUID id, UUID showId, String zone, int seatNumber, double price, String buyer, LocalDateTime soldAt) {
-    public Ticket(UUID showId, String zone, int seatNumber, double price, String buyer) {
-        this(UUID.randomUUID(), showId, zone, seatNumber, price, buyer, LocalDateTime.now());
+public record Ticket(UUID id, Show show, Seat seat, LocalDateTime purchaseTime, BigDecimal price) {
+
+    public Ticket(Show show, Seat seat) {
+        this(UUID.randomUUID(), show, seat, LocalDateTime.now(), seat.zone().getPrice());
     }
 }
---
+++ ooad-challenges-1/ticket-system/src/main/java/com/dsantos/TicketSystem.java
@@ -0,0 +1,8 @@
+package com.dsantos;
+
+import java.util.*;
+import java.time.LocalDateTime;
+
+public class TicketSystem {
+
+}
