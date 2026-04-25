5529abd8c28e29072dd4e9bff63c5a3868b3e734
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-04-25 20:15:00 -0300

    add core constraint annotations

---
+++ code-challenges-1/validation-framework/src/main/java/com/dsantos/validation/constraints/NotEmpty.java
@@ -0,0 +1,14 @@
+package com.dsantos.validation.constraints;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface NotEmpty {
+    String message() default "must not be empty";
+}
