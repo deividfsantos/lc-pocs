596469789e0987e31daff481a91189aa80baa83c
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-05-19 09:15:00 -0300

    init project

---
+++ code-challenges-1/http-server/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    application
+}
+
+group = "com.dsantos"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass = "com.dsantos.http.Main"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
