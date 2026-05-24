878651951275c6ab19781253107ced7b157ccb5d
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-05-24 09:10:00 -0300

    remove products handler

--- code-challenges-1/http-server/src/main/java/com/dsantos/http/Main.java
+++ code-challenges-1/http-server/src/main/java/com/dsantos/http/Main.java
@@ -1,14 +1,12 @@
 package com.dsantos.http;
 
-import com.dsantos.http.handlers.ProductsHandler;
 import com.dsantos.http.handlers.UsersHandler;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Router router = new Router()
             .get("/hello", (req, res) -> res.ok("Hello, World!"))
-            .get("/users", new UsersHandler())
-            .get("/products", new ProductsHandler());
+            .get("/users", new UsersHandler());
 
         Server server = new Server(8080, router);
         server.start();
