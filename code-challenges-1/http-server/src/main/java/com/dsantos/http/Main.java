566872de7053427d643e577a578694d978157eaa
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-05-25 09:45:00 -0300

    add health and status endpoints

--- code-challenges-1/http-server/src/main/java/com/dsantos/http/Main.java
+++ code-challenges-1/http-server/src/main/java/com/dsantos/http/Main.java
@@ -5,6 +5,8 @@ import com.dsantos.http.handlers.UsersHandler;
 public class Main {
     public static void main(String[] args) throws Exception {
         Router router = new Router()
+            .get("/health", (req, res) -> res.json("{\"status\":\"up\"}"))
+            .get("/status", (req, res) -> res.json("{\"version\":\"1.0\",\"uptime\":\"running\"}"))
             .get("/hello", (req, res) -> res.ok("Hello, World!"))
             .get("/users", new UsersHandler());
 
