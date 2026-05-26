6f86f8183f08864e493a842c2031eba4617ad5ae
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-05-26 09:17:00 -0300

    init project structure

---
+++ code-challenges-2/http-stress-test/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    id("application")
+}
+group = "com.dsantos"
+version = "1.0-SNAPSHOT"
+repositories {
+    mavenCentral()
+}
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+application {
+    mainClass = "com.dsantos.Main"
+}
+tasks.test {
+    useJUnitPlatform()
+}
