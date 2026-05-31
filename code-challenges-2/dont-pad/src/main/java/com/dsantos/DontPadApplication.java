466b81ef4ba671dd27c4d33b96799f8d7d68d684
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-05-30 22:47:00 -0300

    add application entry point

---
+++ code-challenges-2/dont-pad/src/main/java/com/dsantos/DontPadApplication.java
@@ -0,0 +1,12 @@
+package com.dsantos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontPadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DontPadApplication.class, args);
+    }
+}
