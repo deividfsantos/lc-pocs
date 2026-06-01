b075da088596670a43076027ed7f552f57565584
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-06-01 10:33:00 -0300

    add pad controller and html page

---
+++ code-challenges-2/dont-pad/src/main/java/com/dsantos/controller/PadController.java
@@ -0,0 +1,14 @@
+package com.dsantos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class PadController {
+
+    @GetMapping("/{padId:[a-zA-Z0-9_-]+}")
+    public String getPad(@PathVariable String padId) {
+        return "forward:/index.html";
+    }
+}
