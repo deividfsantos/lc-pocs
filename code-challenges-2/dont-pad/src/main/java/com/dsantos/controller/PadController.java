75570737d0983c56b984da75e0b67a643f4f73b6
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-06-01 21:19:00 -0300

    fix root path redirect

--- code-challenges-2/dont-pad/src/main/java/com/dsantos/controller/PadController.java
+++ code-challenges-2/dont-pad/src/main/java/com/dsantos/controller/PadController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class PadController {
 
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/home";
+    }
+
     @GetMapping("/{padId:[a-zA-Z0-9_-]+}")
     public String getPad(@PathVariable String padId) {
         return "forward:/index.html";
