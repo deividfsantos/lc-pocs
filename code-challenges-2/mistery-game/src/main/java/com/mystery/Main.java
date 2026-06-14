821f68372be22d1a1c368e705819402025b6bc43
Deivid Santos <deividfranciscosantos@hotmail.com>
2026-06-13 22:17:00 -0300

    init project structure

---
+++ code-challenges-2/mistery-game/src/main/java/com/mystery/Main.java
@@ -0,0 +1,8 @@
+package com.mystery;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("MURDER AT THE RIVERSIDE HOTEL");
+        System.out.println("A murder has been committed. Find the killer.");
+    }
+}
